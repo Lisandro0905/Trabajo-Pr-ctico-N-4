724345bf5d8256c4a59f89e985cd3ce21e2962e4
Lisandro0905 <lisali0905@gmail.com>
2025-06-05 17:52:00 -0300

    Delete EspaciosCadena.java

--- EspaciosCadena.java
+++
@@ -1,9 +0,0 @@
-public class EspaciosCadena{
-
-    public static void main(String[] args){
-
-        String texto = "LasHeras";
-char caracter = texto.charAt(4);
-System.out.println("Caracter en la posición 5: " + caracter);  
-    }
-}
