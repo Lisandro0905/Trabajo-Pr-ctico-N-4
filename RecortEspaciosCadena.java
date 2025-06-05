f582de4718c61ab018aeefb3da003b5e8a7fe9de
Lisandro0905 <lisali0905@gmail.com>
2025-06-05 17:52:38 -0300

    Delete RecortEspaciosCadena.java

--- RecortEspaciosCadena.java
+++
@@ -1,9 +0,0 @@
-public class RecortEspaciosCadena{
-
-    public static void main (String[] args){
-
-        String texto = "                      Hermosa mañana verdad                 ";
-String textoRecortado = texto.trim();
-System.out.println("[" + textoRecortado + "]");
-}
-}
